28749cede2bc3cbf33db89e9e4e366f3bbf05a0e
AgusSubaktiar <43577605+AgusSubaktiar@users.noreply.github.com>
2021-04-15 19:06:56 +0700

    Membuat tampilan table kode gejala

--- app/Views/pages/kode_gejala.php
+++ app/Views/pages/kode_gejala.php
@@ -1,5 +1,5 @@
 <div class="container pt-5">
-    <a href="<?= base_url('data/tambah');?>" class="btn btn-success mb-2">Tambah Data</a>
+    <a href="<?= base_url('kode/tambah');?>" class="btn btn-success mb-2">Tambah Data</a>
     <a href="<?= base_url('pages');?>" class="btn btn-success mb-2">Kembali</a>
     <div class="card">
         <div class="card-header bg-info text-white">
@@ -13,8 +13,6 @@
                             <th>No.</th>
                             <th>Nama Penyakit</th>
                             <th>Nama Gejala</th>
-                            <th>MB</th>
-                            <th>MD</th>
                             <th>Aksi</th>
                         </tr> 
                     </thead>
@@ -22,15 +20,13 @@
                         <?php $no=1; foreach($getData as $isi){?>
                             <tr>
                                 <td><?= $no;?></td>
-                                <td><?= $isi['nama_penyakit'];?></td>
+                                <td><?= $isi['kode_gejala'];?></td>
                                 <td><?= $isi['nama_gejala'];?></td>
-                                <td><?= $isi['MB'];?></td>
-                                <td><?= ($isi['MD']);?></td>
                                 <td>
-                                    <a href="<?= base_url('data/edit/'.$isi['id_basis']);?>" 
+                                    <a href="<?= base_url('kode/edit/'.$isi['id_basis']);?>" 
                                     class="btn btn-success">
                                     Edit</a>
-                                    <a href="<?= base_url('data/hapus/'.$isi['id_basis']);?>" 
+                                    <a href="<?= base_url('kode/hapus/'.$isi['id_basis']);?>" 
                                     onclick="javascript:return confirm('Apakah ingin menghapus data barang ?')"
                                     class="btn btn-danger">
                                     Hapus</a>
